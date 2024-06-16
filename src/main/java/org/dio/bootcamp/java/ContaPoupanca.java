bbc009a0284eeb9fd7929ee8eb5e21a415ba1507
EmersonFlorencio <emersonflorencio@hotmail.com>
2024-06-16 13:06:19 -0300

    feat: Projeto Conta Digital

---
+++ src/main/java/org/dio/bootcamp/java/ContaPoupanca.java
@@ -0,0 +1,15 @@
+package org.dio.bootcamp.java;
+
+public class ContaPoupanca extends Conta{
+
+  public ContaPoupanca(Cliente cliente) {
+    super(cliente);
+  }
+
+  @Override
+  public void imprimirExtrato() {
+    System.out.println("=== Extrato Conta Poupança ===");
+    super.imprimirInfosComuns();
+  }
+
+}
---
+++ src/main/java/org/dio/bootcamp/java/IConta.java
@@ -0,0 +1,11 @@
+package org.dio.bootcamp.java;
+
+public interface IConta {
+  void sacar(double valor);
+
+  void depositar(double valor);
+
+  void transferir(double valor, IConta contaDestino);
+
+  void imprimirExtrato();
+}
